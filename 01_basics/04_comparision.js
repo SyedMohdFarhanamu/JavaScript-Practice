7d974c58cf3c909a0c03935809bb3d6315c6660b
Syed Mohd Farhan <96595862+SyedMohdFarhanamu@users.noreply.github.com>
2024-07-15 07:38:36 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -7,23 +7,23 @@
 // These types of code must be avoided
 1.
 
-// console.log("2" > 1);  // string convert into number
-// console.log("02" > 1);
+console.log("2" > 1);  // string convert into number
+console.log("02" > 1);
 
 2.
 
 // COMPARISION (>,<,>=,<=) & EQUALITY CHECK (==) work differently
 // comparision convert to null to a number and treating it as 0
 // that's why (3) null>=0 is true and null>0 is false
-// console.log(null > 0);  // false
-// console.log(null == 0);  // false
-// console.log(null >= 0);  // true
+console.log(null > 0);  // false
+console.log(null == 0);  // false
+console.log(null >= 0);  // true
 
 3.
 
-// console.log(undefined == 0); //  false
-// console.log(undefined > 0);  //  false
-// console.log(undefined < 0);  //  false
+console.log(undefined == 0); //  false
+console.log(undefined > 0);  //  false
+console.log(undefined < 0);  //  false
 
 // ===  Strictly check also data types
 console.log("2" === 2);  // false
