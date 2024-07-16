9ec63f2ff14b40f16a411578428ccf20b65ab91c
Syed Mohd Farhan <96595862+SyedMohdFarhanamu@users.noreply.github.com>
2024-07-16 11:30:45 +0000

    num and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -19,3 +19,23 @@ console.log(typeof otherNumber.toPrecision(3)); // return into STRING
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN')); // TO CONVERT INDIAN NUMBER SYSTEM
+
+//  ++++++++++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));  // UPPER value
+console.log(Math.floor(4.9));  //  LOWER value
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
